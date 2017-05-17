931ff5fb627719ad0029790c73422d24007027c4
iqbalfn <iqbalfawz@gmail.com>
2017-05-17 11:45:03 +0700

    Increase original column table width

--- modules/upload/_db/install.sql
+++ modules/upload/_db/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `media` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(42) NOT NULL,
-    `original` VARCHAR(100) NOT NULL,
+    `original` VARCHAR(250) NOT NULL,
     `mime` VARCHAR(30) NOT NULL,
     `path` VARCHAR(60) NOT NULL,
     `form` VARCHAR(50) NOT NULL,

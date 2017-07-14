dd375458703be5fa1af4f1102e7a9a1b793f8589
iqbalfn <iqbalfawz@gmail.com>
2017-07-14 10:50:00 +0700

    Remove table partition

--- modules/upload/_db/install.sql
+++ modules/upload/_db/install.sql
@@ -1,18 +1,10 @@
 CREATE TABLE IF NOT EXISTS `media` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(42) NOT NULL,
     `original` VARCHAR(250) NOT NULL,
     `mime` VARCHAR(30) NOT NULL,
     `path` VARCHAR(60) NOT NULL,
     `form` VARCHAR(50) NOT NULL,
     `user` INTEGER,
-    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    PRIMARY KEY(
-        `id`,
-        `original`
-    )
-)
-
-    PARTITION BY KEY(`original`)
-    PARTITIONS 50;
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
